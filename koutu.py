f20b4b756b2b32504f3703832a7fd26e6968224b
jinx <412001580@qq.com>
2023-03-20 22:06:26 +0800

    format

--- koutu.py
+++ koutu.py
@@ -5,14 +5,39 @@ import paddlehub as hub
 from PIL import Image, ImageSequence
 import numpy as np
 import os
+import sys
+import imghdr
+
+
 
 # 测试图片路径和输出路径
 test_path = ''  #按需更改路径
 output_path = ''  #按需更改路径 
+input_path='./'
+files = []
+dirs = os.listdir(input_path)
+typeList={'.jpg' , '.png' , '.jpeg' , 'webm'}
+for diretion in dirs:
+    # imgType = imghdr.what(diretion)
+    
+    # print(imgType)
+    fileType =os.path.splitext(diretion)[-1]
+    # print(fileType)
+    if  fileType in typeList:
+      files.append(input_path + diretion)
+      print(fileType ,"存在")
+    # else :
+      # print(fileType ,"不存在")
+    # if fileType == '.jpg' or '.png' or 'jpeg' or 'webm' :
+    #   print(fileType)
+    
+
+    # files.append(input_path + diretion)
 
 # 待预测图片
-test_img_path = ["2.png"]  #按需更改文件名
-test_img_path = [test_path + img for img in test_img_path]
+test_img_path = ["1.jpeg"]  #按需更改文件名
+# test_img_path = [test_path + img for img in test_img_path]
+test_img_path = [test_path + img for img in files]
 
 out_img_path = 'humanseg_output'+os.sep + os.path.basename(test_img_path[0]).split('.')[0] + '.png'
 out_path = 'humanseg_output'
@@ -30,10 +55,10 @@ for result in results:
 
 # 预测结果展示
 # out_img_path = 'zhou.png'   #输出图片的位置
-print(out_img_path)
+# print(out_img_path)
 
-img = mpimg.imread(out_img_path)
-plt.figure(figsize=(10,10))
-plt.imshow(img)
+# img = mpimg.imread(out_img_path)
+# plt.figure(figsize=(10,10))
+# plt.imshow(img)
 plt.axis('off')
-plt.show()
+# plt.show()
